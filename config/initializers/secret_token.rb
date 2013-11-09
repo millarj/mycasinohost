935a1e8e30ad8ba529074563ec4698af90b84794
Jeff Millar <jeff.millar@sbcglobal.net>
2013-11-08 21:13:40 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mycasinohost::Application.config.secret_key_base = '8cde4126991a59c7283fab9f2da0ac4d54566e141f7c4ce14f6a6479a1e3393b5cfac823d83a7746cded4bd424c76af0cc90835ccec7951e06067041836b16be'
